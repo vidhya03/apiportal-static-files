cc4ee34cc8ade65a9da89fe898708831ed577af8
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-04-11 21:46:40 +0530

    added upto toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,53 @@
-$(".content-type").click(function (event) {
-    console.log("clicked");
+$(".content-type").click(function (event) {   
+    var contentType = $(this);       
     $(".content-type").next().hide();
+    $(".content-type-selected").removeClass("content-type-selected");
+    $(this).addClass("content-type-selected");
+    var groupContentTypeElement = $(this).next();
+    groupContentTypeElement.show();
+    /* 
+    Get the first element position left and assign to it.
     
-    $(this).next().show();
-    $(this).next().addClass("")
-    // $(this).prevUntil(".group-content-type").css("width:115px;");
-    $(this).prev().css("background","red");
-    $(this).prev().hide();
+    or set the default position left to 2
+    */    
+    var offset = groupContentTypeElement.offset();
+    //set the position
+    groupContentTypeElement.offset({ top: offset.top, left: 2 });
 });
 
-  $(".group-schema-example").hide();
+
+$(".header-example-schema").click(function (event) {   
+    var headerExampleSchema = $(this);     
+    var isActivated = headerExampleSchema.hasClass("header-example-schema-activated");
+    if(isActivated){
+        return;
+    }
+   
+
+    headerExampleSchema.parent()
+   .closest(".schema-or-example").find(".header-example-schema");
+
+   headerExampleSchema.parent().parent()
+   .closest(".schema-or-example").find(".header-example-schema")
+   .removeClass("header-example-schema-activated");
+   
+   //$(".header-example-schema-activated").removeClass("header-example-schema-activated");
+    headerExampleSchema.addClass("header-example-schema-activated");
+
+    var schemaExample = $(this).next();    
+    schemaExample.show();
+
+    var offset = schemaExample.offset();
+    //set the position
+    schemaExample.offset({ top: offset.top, left: 2 });
+    
+});
+
+$(".group-schema-example").hide();
+$(".header-example-schema").next().hide();
+$(".header-example-schema.header-example-schema-activated").next().show();
 
 
-  $(".featured.module").filter(function(index, element){
+$(".featured.module").filter(function (index, element) {
     return index % 3 == 2;
 }).addClass("third");
